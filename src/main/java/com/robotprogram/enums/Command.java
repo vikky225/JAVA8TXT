5e8d56e576596f0452e230c19190e840b635ec1a
vikas <vikky225@gmail.com>
2019-11-11 22:20:37 +1030

    Revert "Robot Assignment commit"
    This reverts commit da986500

--- src/main/java/com/robotprogram/enums/Command.java
+++
@@ -1,9 +0,0 @@
-package com.robotprogram.enums;
-
-public enum Command {
-    PLACE,
-    MOVE,
-    LEFT,
-    RIGHT,
-    REPORT
-}
--- src/main/java/com/robotprogram/exception/RobotException.java
+++
@@ -1,7 +0,0 @@
-package com.robotprogram.exception;
-
-public class RobotException extends Exception{
-    public RobotException(String string) {
-        super(string);
-    }
-}
